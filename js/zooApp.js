96b608d17cc82b051eba09d669d2975105214732
Katja Berčič <katja.bercic@gmail.com>
2016-01-23 11:16:38 +0100

    Add property selection functionality

--- js/zooApp.js
+++ js/zooApp.js
@@ -1,19 +1,48 @@
-var app = angular.module("zoo", ['ngMaterial', 'ui.grid'])
+(function () {
 
-app.controller('ZooCtrl', ['$scope', function ($scope) {
+  'use strict'
 
-	$scope.objects = [
-		{	"order": 10,
-			"name": "Petersen",
-			"p1": 2,
-			"p2": "yes",
-			"p3": 123 },
+	angular
+      .module("zoo", ['ui.bootstrap', 'ui.grid', 'ui.scroll'])
+      .controller('ZooCtrl', ZooCtrl)
 
-		{	"order": 8,
-			"name": "cube",
-			"p1": 25,
-			"p2": "no",
-			"p3": 734 }
+	function ZooCtrl ($timeout, $q) {
+    var self = this;
+
+    self.properties = [
+			{name: 'bipartite', dbName: 'is_bipartite', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'Cayley', dbName: 'is_cayley', type: 'Numeric', selected: false, conditionB: true, edit: false},
+			{name: 'diameter', dbName: 'diameter', type: 'Numeric', selected: false, conditionB: true, edit: false},
+			{name: 'girth', dbName: 'girth', type: 'Numeric', selected: false, conditionN: '', edit: false},
+			{name: 'Moebius ladder', dbName: 'is_moebius_ladder', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'odd girth', dbName: 'odd_girth', type: 'Numeric', selected: false, conditionB: true, edit: false},
+			{name: 'order', dbName: 'order', type: 'Numeric', selected: false, conditionB: true, edit: false},
+			{name: 'prism', dbName: 'is_prism', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'size', dbName: 'size', type: 'Numeric', selected: false, conditionN: '', edit: false}, // #edges
+			{name: 'SPX', dbName: 'is_spx', type: 'Boolean', selected: false, conditionB: true, edit: false}
 		]
+		self.numericPropertyValidator = /^(=|==|<=|>=|<|>)\s*(\d+\.?\d*)$|^([\[\(])\s*(\d+\.?\d*),?\s*(\d+\.?\d*)\s*([\]\)])$|^((\d+\.?\d*,?\s*)+)$/
 
-	}])
+		self.propertyEditSwitch = function(property) {
+			var index = self.properties.indexOf(property);
+			if (index > -1) {
+				property.edit = !property.edit
+				self.properties[index] = property
+			}
+		}
+
+		self.propertySelectSwitch = function(property) {
+			var index = self.properties.indexOf(property);
+			if (index > -1) {
+				property.selected = !property.selected
+				self.properties[index] = property
+			}
+		}
+
+		self.submitSearch = function() {
+			var selectedProperties = self.properties.filter(function (property) { return property.selected });
+			console.log(selectedProperties)
+		}
+  }
+
+})();
