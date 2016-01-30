f7f5cf55f61196065952e1f43fdd2e0803769f26
Katja Berčič <katja.bercic@gmail.com>
2016-01-30 17:52:30 +0100

    Update server communication

--- js/zooApp.js
+++ js/zooApp.js
@@ -10,30 +10,26 @@
     var self = this;
 
     self.properties = [
+			{name: 'arc transitive', dbName: 'is_arc_transitive', type: 'Boolean', selected: false, conditionB: true, edit: false},
 			{name: 'bipartite', dbName: 'is_bipartite', type: 'Boolean', selected: false, conditionB: true, edit: false},
 			{name: 'Cayley', dbName: 'is_cayley', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'clique number', dbName: 'clique_number', type: 'Numeric', selected: false, conditionB: true, edit: true},
 			{name: 'diameter', dbName: 'diameter', type: 'Numeric', selected: false, conditionB: true, edit: true},
+			{name: 'distance regular', dbName: 'is_distance_regular', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'distance transitive', dbName: 'is_distance_transitive', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'edge transitive', dbName: 'is_edge_transitive', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'Eulerian', dbName: 'is_eulerian', type: 'Boolean', selected: false, conditionB: true, edit: false},
 			{name: 'girth', dbName: 'girth', type: 'Numeric', selected: false, conditionN: '', edit: true},
 			{name: 'Moebius ladder', dbName: 'is_moebius_ladder', type: 'Boolean', selected: false, conditionB: true, edit: false},
 			{name: 'odd girth', dbName: 'odd_girth', type: 'Numeric', selected: false, conditionB: true, edit: true},
 			{name: 'order', dbName: 'order', type: 'Numeric', selected: false, conditionB: true, edit: true},
 			{name: 'prism', dbName: 'is_prism', type: 'Boolean', selected: false, conditionB: true, edit: false},
-			{name: 'SPX', dbName: 'is_spx', type: 'Boolean', selected: false, conditionB: true, edit: false}
+			{name: 'split', dbName: 'is_split', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'strongly regular', dbName: 'is_strongly_regular', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'SPX', dbName: 'is_spx', type: 'Boolean', selected: false, conditionB: true, edit: false},
+			{name: 'triangles count', dbName: 'triangles_count', type: 'Numeric', selected: false, conditionB: true, edit: true}
 		]
 		self.numericPropertyValidator = /^(=|==|<=|>=|<|>|<>|!=)\s*(\d+\.?\d*)$|^([\[\(])\s*(\d+\.?\d*),?\s*(\d+\.?\d*)\s*([\]\)])$|^((\d+\.?\d*,?\s*)+)$/
-
-// - clique_number
-// - is_arc_transitive
-// - is_distance_regular
-// - is_distance_transitive
-// - is_edge_transitive
-// - is_eulerian
-// - is_overfull
-// - is_split
-// - is_strongly_regular
-// - is_vertex_transitive
-// - size (število povezav)
-// - triangles_count
 
 		self.displayResults = false
 		self.counter = 0
